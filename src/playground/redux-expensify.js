2c3a48ceb747cb23c0c82a929343dea6d91be8d8
Simon-Oliver <stauffer.simon@outlook.com>
2018-12-31 15:29:42 +1100

    Added EDIT_EXPENSE action

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,4 +1,98 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
+
+// ADD_EXPENSE
+const addExpense = ({
+  description = '',
+  note = '',
+  amount = 0,
+  createdAt = 0
+} = {}) => ({
+  type: 'ADD_EXPENSE',
+  expenses: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+});
+
+// REMOVE_EXPENSE
+
+const removeExpense = ({ id } = {}) => ({
+  type: 'REMOVE_EXPENSE',
+  id
+});
+
+// EDIT_EXPENSE
+const editExpense = (id, update) => ({
+  type: 'EDIT_EXPENSE',
+  id,
+  update
+});
+
+// Expenses Reducer
+
+const expensesDefault = [];
+const expensesReducer = (state = expensesDefault, action) => {
+  switch (action.type) {
+    case 'ADD_EXPENSE':
+      return [...state, action.expenses];
+    case 'REMOVE_EXPENSE':
+      return state.filter(e => e.id !== action.id);
+    case 'EDIT_EXPENSE':
+      return state.map(e => {
+        if (e.id === action.id) {
+          return {
+            ...e,
+            ...action.update
+          };
+        }
+        return e;
+      });
+    default:
+      return state;
+  }
+};
+
+// Filter Reducer
+const filtersDefault = {
+  text: '',
+  sortBy: 'date', // date or amount
+  startDate: undefined,
+  endDate: undefined
+};
+
+const filtersReducer = (state = filtersDefault, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+// Store creation
+
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filtersReducer
+  })
+);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+const expenseOne = store.dispatch(
+  addExpense({ description: 'TEST', amount: 100 })
+);
+const expenseTwo = store.dispatch(
+  addExpense({ description: 'COFFE', amount: 300 })
+);
+
+store.dispatch(removeExpense({ id: expenseOne.expenses.id }));
+store.dispatch(editExpense(expenseTwo.expenses.id, { amount: 500 }));
 
 const demoState = {
   expenses: [
