74967595c01bbb63dfc67adf6e08e1c3ab39a38c
Simon-Oliver <stauffer.simon@outlook.com>
2019-01-04 12:45:31 +1100

    creat expense and dashboard redirect implemented

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
+import { addExpense } from '../actions/expenses';
 
-const AddExpensePage = () => (
+const AddExpensePage = props => (
   <div>
     <h1>Add Expense</h1>
-    <ExpenseForm />
+    <ExpenseForm
+      onSubmit={e => {
+        props.dispatch(addExpense(e));
+        props.history.push('/');
+      }}
+    />
   </div>
 );
 
-export default AddExpensePage;
+export default connect()(AddExpensePage);
